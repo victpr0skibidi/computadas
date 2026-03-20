e2e6e7a537c1cdd1162dc932084f9d0d6c8e2d1c
victpr0skibdi <victormillnitz@gmail.com>
2026-03-20 15:15:17 -0300

    20.03 | 15:15

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@ const nome = ref('')
 const sobrenome= ref('')
 
 const nomeCompleto= () => {
-  return `$(nome)`
-}
+  console.log('Calculando nome com função')
+  return `${nome.value} ${sobrenome.value}`
+  }
 </script>
 
 <template>
@@ -13,6 +14,7 @@ const nomeCompleto= () => {
     <input type="text" v-model1="nome" placeholder="Nome">
     <input type="text" v-model1="sobrenome" placeholder="Sobrenome">
     <label> {{ nomeCompleto() }}</label>
+    <input type="text" v-model="texto">
 
   </div>
 </template>

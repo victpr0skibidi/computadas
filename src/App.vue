5bb9eb1c91fd8a2d1cee5fc745c4eacd60637d57
victpr0skibdi <victormillnitz@gmail.com>
2026-03-20 15:10:38 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+const nome = ref('')
+const sobrenome= ref('')
+
+const nomeCompleto= () => {
+  return `$(nome)`
+}
+</script>
+
+<template>
+  <div class="conteiner">
+    <input type="text" v-model1="nome" placeholder="Nome">
+    <input type="text" v-model1="sobrenome" placeholder="Sobrenome">
+    <label> {{ nomeCompleto() }}</label>
+
+  </div>
+</template>
+
+<style scoped>
+</style>
